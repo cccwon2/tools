cb0cc59eedb2ec8156493b8ce6407644c8971356
cccwon2 <cccwon2@naver.com>
2025-07-03 16:10:00 +0900

    GA 태그 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <title>도구 모음</title>
         <meta name="description" content="유용한 도구들을 모아놓은 웹사이트" />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9XM27FLSHV"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-9XM27FLSHV');
+            `,
+          }}
+        />
       </head>
       <body>{children}</body>
     </html>
